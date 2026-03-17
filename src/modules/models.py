b8616cf8a8fb2676b9509e261eb9b1cf84eb62e4
dKaustav43 <dkaustav43@gmail.com>
2026-03-17 23:13:45 +0530

    Removed unwanted code

--- src/modules/models.py
+++ src/modules/models.py
@@ -8,12 +8,6 @@ class Models(SQLModel, table=True):
 
     outputs: list["Outputs"] = Relationship(back_populates="models")
 
-# class Prompts(SQLModel, table=True):
-#     id: int | None = Field(default=None, primary_key=True)
-#     prompt: str
-
-#     outputs: list["Outputs"] = Relationship(back_populates="prompts")
-
 class CaseStudyTexts(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     text: str
@@ -24,9 +18,7 @@ class Outputs(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     output: str | None = Field(default=None)
     model_id: int | None = Field(default=None, foreign_key="models.id")
-    # prompt_id: int | None = Field(default=None, foreign_key="prompts.id")
     text_id: int |  None = Field(default=None, foreign_key="casestudytexts.id")
 
     models: Models | None = Relationship(back_populates="outputs")
-    # prompts: Prompts | None = Relationship(back_populates="outputs")
     texts: CaseStudyTexts | None = Relationship(back_populates="outputs")
