17f673bc39ff5276c5c44f6f56bb280ad3ac0c42
dKaustav43 <dkaustav43@gmail.com>
2026-03-17 11:35:22 +0530

    updated the output with user prompt + text input from newly created caserstudy text table

--- application/main.py
+++ application/main.py
@@ -11,6 +11,18 @@ with open("data/llm_models.json", mode="r", encoding="utf-8") as read_file:
 with open("data/case_Study_texts.json", mode="r", encoding="utf-8") as read_file:
         texts_data_list = json.load(read_file)
 
+#text summarisation user query
+text_summarisation_query = """
+Summarise the following case study from one of the Catapults based in the UK in 150 words. 
+
+- No need to include the title.
+- Include Catapult innovation support, the outcome and impact of the project if present. 
+- Include UK government departments if any the case study.
+- Include company/business names if mentioned.
+- Include region, international information if mentioned.
+
+"""
+
 #add data to the tables
 def create_model_and_prompts():
 
@@ -35,7 +47,7 @@ def create_casestudy_texts():
         session.commit() 
 
 #generate summaries using models and prompts in Ollama and add them to the table
-def create_outputs():
+def create_outputs(user_query:str = text_summarisation_query):
     with Session(engine) as session:
         models_table_data = session.exec(select(Models)).all()
         texts_data_list = session.exec(select(CaseStudyTexts)).all()
@@ -45,8 +57,8 @@ def create_outputs():
             for llm_model in models_table_data: 
                 existing = session.exec(select(Outputs).where(Outputs.model_id == llm_model.id).where(Outputs.text_id == t.id)).one_or_none()
                 if not existing:
-                    response = generate(llm_model.model,t.text)
-                    text_summarisation_entry = Outputs(output=str(response),
+                    response = generate(llm_model.model,user_query + str(t.text))
+                    text_summarisation_entry = Outputs(output=str(response.response),
                                                     models=llm_model,
                                                     texts=t)
                     session.add(text_summarisation_entry)
@@ -66,7 +78,7 @@ def select_model():
 def select_texts():
     with Session(engine) as session:
         texts_first_entry = session.exec(select(CaseStudyTexts)).first()
-        print(texts_first_entry)
+        print(text_summarisation_query + str(texts_first_entry.text)) # type: ignore
     
 
 #Read outputs and write to a .txt file
@@ -85,14 +97,14 @@ def select_outputs():
         print("outputs appended")
 
 #updating the outputs table with only the response object and nothing else
-def update_outputs():
+def update_outputs(user_query:str = text_summarisation_query):
      with Session(engine) as session:
         models_table_data = session.exec(select(Models)).all()
         case_study_texts = session.exec(select(CaseStudyTexts)).all()
         
         for t in case_study_texts:
             for llm_model in models_table_data: 
-                response = generate(llm_model.model,t.text)
+                response = generate(llm_model.model, user_query + str(t.text))
                 outputs_table = session.exec(select(Outputs).where(Outputs.model_id == llm_model.id).where(Outputs.text_id == t.id)).one()
                 outputs_table.output = str(response.response) 
                 session.add(outputs_table)
@@ -104,7 +116,7 @@ def main():
     create_db_and_tables()
     #create_casestudy_texts()
     #create_outputs()
-    #update_outputs()
+    update_outputs()
     #select_outputs()
     #select_texts()
 
